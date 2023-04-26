5a2763b2a5bb46db40d06f99a250404321e35101
TYLPHE <tylphet@gmail.com>
2023-04-25 22:07:16 -0600

    js-demo.html now has functionality

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 'use strict';
+const button = document.querySelector('button.color-changer');
+button.addEventListener('click', () => {
+  const elements = document.querySelectorAll('.color-change');
+  for (const element of elements) {
+    element.classList.add('red');
+  }
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const numInput = Number(document.querySelector('#number-input').value);
+  const formFeedback = document.querySelector('#formFeedback');
+  if (isNaN(numInput) || numInput >= 10) {
+    formFeedback.textContent = 'Please enter a smaller number.';
+  } else {
+    formFeedback.textContent = 'You are good to go!'
+  }
+})
